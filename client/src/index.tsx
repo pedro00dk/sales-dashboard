c8c3f27fbb90612cb9bb503f0d25065be56dcd4f
Pedro Henrique <pedro00dk@gmail.com>
2021-02-02 16:23:45 -0300

    Create basic react project

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+// import 'regenerator-runtime/runtime'
+
+const root$ = document.getElementById('root')
+ReactDom.render(<div>{'hello world!'}</div>, root$)
