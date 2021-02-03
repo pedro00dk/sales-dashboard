3180a180cf09957e354ec2544b8e2b6f1da84e83
Pedro Henrique <pedro00dk@gmail.com>
2021-02-03 11:27:13 -0300

    Consume store menu state to configure menu navigation in Container

--- client/src/view/Container.tsx
+++ client/src/view/Container.tsx
@@ -1,24 +1,39 @@
 import * as React from 'react'
+import { actions, DefaultStore, useDispatch, useSelection } from '../state/Store'
 import './Container.css'
 import { HomeMenu } from './menu/HomeMenu'
 import { UserDetail } from './menu/UserDetail'
 import { UsersMenu } from './menu/UsersMenu'
 import { MenuBar } from './MenuBar'
 
-const menuComponents = { HOME: HomeMenu, 'USER LIST': UsersMenu }
-const menuNames = Object.keys(menuComponents)
+export const Container = () => (
+    <DefaultStore>
+        <Content></Content>
+    </DefaultStore>
+)
 
-export const Container = () => {
-    const [selectedMenu, setSelectedMenu] = React.useState(menuNames[0])
-    const MenuComponent = menuComponents[selectedMenu]
+export const menus = { HOME: HomeMenu, 'USER LIST': UsersMenu, 'USER DETAIL': UserDetail }
+const menuBarNames = ['HOME', 'USER LIST']
+
+export const Content = () => {
+    const dispatch = useDispatch()
+    const { menu } = useSelection(state => ({ menu: state.menu }))
+
+    if (menu.length === 0) dispatch(actions.menu.push(menuBarNames[0]))
+    const menuName = menu.length !== 0 ? menu[menu.length - 1].name : ''
+    const Component = menu.length !== 0 ? menus[menu[menu.length - 1].name] : HomeMenu
+    const componentData = menu.length !== 0 ? menu[menu.length - 1].data : undefined
+
+    React.useEffect(() => {
+        dispatch(actions.menu.set(menuBarNames[0]))
+    }, [])
 
     return (
         <div className='container'>
             <Header />
             <div className='container-content'>
-                <MenuBar menus={menuNames} selected={selectedMenu} onClick={setSelectedMenu} />
-                {/* <MenuComponent /> */}
-                <UserDetail />
+                <MenuBar menus={menuBarNames} selected={menuName} />
+                <Component />
             </div>
             <Footer />
         </div>
