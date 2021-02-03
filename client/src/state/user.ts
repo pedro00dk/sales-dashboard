38ca3c29d171238092104b31372c834b7ea70451
Pedro Henrique <pedro00dk@gmail.com>
2021-02-03 14:56:55 -0300

    Add mockup data to user reducer

--- client/src/state/user.ts
+++ client/src/state/user.ts
@@ -4,13 +4,252 @@
 
 import { User } from './types/User'
 
-type State = { users: { [username: string]: User }; topSellers: User[]; selectedUser?: User }[]
+const users: User[] = [
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 123123125.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+            {
+                product: 'French Fries',
+                date: new Date(),
+                unitValue: 5.45,
+                volume: 10000,
+            },
+        ],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+    {
+        name: 'Pedro',
+        username: 'phsm',
+        phone: '(81) 99508-8741',
+        totalSales: 1234,
+        sales: [],
+    },
+    {
+        name: 'Guilherme',
+        username: 'ghps',
+        phone: '(81) 99999-9999',
+        totalSales: 4321,
+        sales: [],
+    },
+]
+
+const x = users.reduce((acc, next, i) => {
+    acc[next.username + i.toString()] = next
+    return acc
+}, {} as { [username: string]: User })
+
+type State = { users: { [username: string]: User }; topSellers: User[]; selectedUser?: User }
 
 type Action =
     | { type: 'user/fetch'; payload?: { user: User }; error?: Error } // TODO implement
     | { type: 'user/select'; payload: { user: User } }
 
-const initialState: State = []
+const initialState: State = {
+    users: x,
+    topSellers: users,
+    selectedUser: undefined,
+}
 
 export const reducer = (state = initialState, action: Action) => {
     switch (action.type) {
