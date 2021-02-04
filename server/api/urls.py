28378cff86344469e4568b3b84a3edf508269b67
Pedro Henrique <pedro00dk@gmail.com>
2021-02-04 11:15:09 -0300

    Fix missing stats url

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,12 +2,14 @@ from django import urls
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import SaleView, UserView
+from .views import BestSellersView, SaleView, StatsView, UserView
 
 router = routers.DefaultRouter()
 router.register('users', UserView)
+router.register('best_sellers', BestSellersView)
 router.register('sales', SaleView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('stats/', StatsView.as_view()),
 ]
