838ab77ccfc094d0fd2f27fc4d6ad32fc860241e
Pedro Henrique <pedro00dk@gmail.com>
2021-02-04 14:18:36 -0300

    User serializer now also produces sales objects

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -4,19 +4,7 @@ from rest_framework import serializers
 from .models import Sale, User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-
-    totalSales = serializers.SerializerMethodField('get_total_sales')
-    def get_total_sales(self, model):
-        return model.total_sales
-
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'name', 'phone', 'total_sales', 'totalSales')
-
-
 class SaleSerializer(serializers.ModelSerializer):
-
     unitPrice = serializers.SerializerMethodField('get_unit_price')
     def get_unit_price(self, model):
         return model.unit_price
@@ -24,3 +12,15 @@ class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = ('url', 'id', 'user', 'product', 'volume', 'unit_price', 'date', 'unitPrice')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    sales = SaleSerializer(source='sale_set', many=True)
+
+    totalSales = serializers.SerializerMethodField('get_total_sales')
+    def get_total_sales(self, model):
+        return model.total_sales
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'name', 'phone', 'total_sales', 'totalSales', 'sales')
