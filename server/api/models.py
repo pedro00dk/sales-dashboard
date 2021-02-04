e1f9908cd3005e1307650329a21cdd97b53925bf
Pedro Henrique <pedro00dk@gmail.com>
2021-02-04 10:04:52 -0300

    Sale model now updates User.total_sale when created

--- server/api/models.py
+++ server/api/models.py
@@ -5,7 +5,10 @@ class User(models.Model):
     username = models.CharField(max_length=256, primary_key=True)
     name = models.CharField(max_length=256)
     phone = models.CharField(max_length=32)
-    totalSales = models.FloatField()
+    total_sales = models.FloatField()
+
+    def __str__(self):
+        return f'{type(self).__name__} ({self.username}, {self.total_sales})'
 
 
 class Sale(models.Model):
@@ -14,3 +17,11 @@ class Sale(models.Model):
     volume = models.IntegerField()
     unit_price = models.FloatField()
     date = models.DateField()
+
+    def __str__(self):
+        return f'{type(self).__name__} [{self.user.username}] ({self.product}, {self.date})'
+
+    def save(self, *args, **kwargs):
+        self.user.total_sales += self.unit_price * self.volume
+        super(Sale, self).save(*args, **kwargs)
+        self.user.save()
